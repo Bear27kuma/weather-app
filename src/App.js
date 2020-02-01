30132691e390b190693d7daee0b7d0e68b5650a7
Your Name <y.dream.nest@icloud.com>
2020-02-01 16:09:06 +0900

    Complete Basic Functions

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "6051da97b1c1968c8cfe8dbf5d7f91d3";
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      // icon: data.weather[0].icon,
+      error: ""
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          // icon={this.state.icon}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
+};
+
+export default App;
